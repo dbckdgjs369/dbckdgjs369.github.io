1d20ee52b41dd84dd23a652271635c2ad7098dd5
yoo chang heon <dbckdgjs1130@naver.com>
2022-10-20 23:30:11 +0900

    feat: layout 및 위로 버튼 추가

--- Blog/src/components/layout.js
+++ Blog/src/components/layout.js
@@ -11,18 +11,50 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 import styled from "@emotion/styled"
+import UpperArrow from "../images/arrow.png"
 
 const Wrapper = styled.div`
   margin: 0 auto;
+  font-size: 1rem;
   max-width: 54rem;
   padding: 32px;
 `
 const Footer = styled.footer`
   margin-top: 32px;
   font-size: 14px;
-  position: fixed;
   bottom: 0;
-  margin-bottom: 5rem;
+  margin-top: 5rem;
+`
+
+const Main = styled.main`
+  line-height: 2.4rem;
+  //여기에서 본문 markdown 스타일 수정
+  font-size: 1.2rem;
+  color: black;
+
+  a {
+    color: gray !important;
+    text-decoration: none !important;
+  }
+  a:hover {
+    color: rgb(75, 74, 74) !important;
+  }
+`
+const TopBtn = styled.button`
+  position: fixed;
+  bottom: 5rem;
+  right: 5rem;
+  width: 5rem;
+  height: 5rem;
+  border-radius: 100%;
+  font-size: 1.3rem;
+  border: none;
+  box-shadow: 1px 1px 1px 1px gray;
+`
+
+const Arrow = styled.img`
+  height: 2.2rem;
+  margin: 0;
 `
 
 const Layout = ({ children }) => {
@@ -40,7 +72,11 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <Wrapper>
-        <main>{children}</main>
+        <TopBtn>
+          <Arrow src={UpperArrow} />
+          Top
+        </TopBtn>
+        <Main>{children}</Main>
         <Footer>
           © {new Date().getFullYear()} &middot; Groot Inc. All rights reserved.
         </Footer>
